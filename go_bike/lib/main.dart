deaa2a6157b2f2172b49b4ef6257b80e107bd19e
Thiago Ramos <thiagorbernardo@hotmail.com>
2022-09-17 20:25:03 -0300

    feat: init notify

--- go_bike/lib/main.dart
+++ go_bike/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_bike/data/bloc/user/user_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,11 +7,37 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 
 import 'package:go_bike/cubit/bottom_nav_cubit.dart';
 import 'package:go_bike/cubit/theme_cubit.dart';
 import 'package:go_bike/utils/routes/routes.dart';
+import 'package:go_bike/data/bloc/user/user_bloc.dart';
+import 'package:go_bike/models/push_notification.dart';
+
+void requestAndRegisterNotification() async {
+  NotificationSettings settings = await FirebaseMessaging.instance
+      .requestPermission(
+          alert: true, badge: true, provisional: false, sound: true);
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    print("\n\n\n\n TOKEN $fcmToken \n\n\n\n");
+    //TODO: send to server
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+
+      PushNotification _notification = PushNotification(
+          title: message.notification!.title, body: message.notification!.body);
+    });
+  }
+}
 
 void main() async {
   /// Initialize packages
@@ -22,6 +47,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  requestAndRegisterNotification();
   // Storage
   Hive.initFlutter();
 
--- go_bike/macos/Flutter/GeneratedPluginRegistrant.swift
+++ go_bike/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import firebase_messaging
 import geolocator_apple
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
